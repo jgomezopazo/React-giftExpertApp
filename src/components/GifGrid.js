13b1837e4f91af1336c48ed5a70168054588afea
Jorge Gomez <jorgegom@gmail.com>
2021-01-28 20:08:07 -0300

    First Commit

---
+++ src/components/GifGrid.js
@@ -0,0 +1,28 @@
+import { React } from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GiftGridItem } from './GiftGridItem';
+
+export const GifGrid = ( { category } ) => {
+
+    const { data:images, loading } = useFetchGifs(category);
+    
+    return (
+      <>
+        <h3 className='animate__animated animate__fadeIn'> {category} </h3>
+        { loading ? <p className='animate__animated animate__flash'> Cargando</p>: null }
+        
+
+        { <div className='card-grid'>
+            {
+                images.map( img => 
+                    <GiftGridItem 
+                        key={img.id} 
+                        { ...img }
+                    /> 
+                )
+                  
+            }
+        </div> }
+      </>
+    );
+}
---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { getGifs } from '../helpers/GetGif';
+
+export const useFetchGifs = (category) => {
+
+  const [state, setState] = useState({
+    data: [],
+    loading: true,
+  });
+
+  useEffect(() => {
+    getGifs(category).then((imgs) => {
+      setTimeout(() => {
+        setState({
+          data: imgs,
+          loading: false,
+        });
+      }, 1000);
+    });
+  }, [category]);
+
+  return state;
+};
